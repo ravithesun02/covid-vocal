d7de1817aa59cc060aad7a53d9a1edb4bc24e796
Ravi Kumar <ravithesun02@gmail.com>
2021-03-14 00:28:53 +0530

    chnage

--- webpack.config.client.production.js
+++ webpack.config.client.production.js
@@ -7,9 +7,9 @@ const config = {
         path.join(CURRENT_WORKING_DIR, 'client/main.js')
     ],
     output: {
-        path: path.join(CURRENT_WORKING_DIR , '/dist'),
+        path: path.join(CURRENT_WORKING_DIR , '/public'),
         filename: 'bundle.js',
-        publicPath: "/dist/"
+        publicPath: "/public/"
     },
     module: {
         rules: [
