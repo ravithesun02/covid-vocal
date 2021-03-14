3d1907a1ad3db35f3b728048b47e4667c5ea89da
Ravi Kumar <ravithesun02@gmail.com>
2021-03-14 10:53:18 +0530

    Fixed mobile view

--- template.js
+++ template.js
@@ -2,7 +2,7 @@ export default ({markup,css})=>{
     return `<!doctype html>
     <html lang="en">
     <head>
-    <meta charset="utf-8">
+    <meta charset="utf-8" name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet"
     href="https://fonts.googleapis.com/css?family=Roboto:100,300,400">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons"
