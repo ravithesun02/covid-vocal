455285fddf7ab384fdf919792bd532b65aad1e0c
Ubuntu <ubuntu@ip-172-31-29-12.us-west-1.compute.internal>
2021-04-24 15:32:06 +0000

    serve and https

--- server/express.js
+++ server/express.js
@@ -48,7 +48,8 @@ app.use(express.urlencoded({ limit:'50mb', extended: false }))
 // app.use(helmet());
 // enable CORS - Cross Origin Resource Sharing
 app.use(cors())
-
+app.use(express.static(__dirname,{dotfiles:'allow'}));
+app.use('/.well-known',express.static(path.join(CURRENT_WORKING_DIR, 'public/.well-known')));
 app.use('/public', express.static(path.join(CURRENT_WORKING_DIR, 'public')))
 
 app.use('/api',predictRouter);
--- server/server.js
+++ server/server.js
@@ -1,6 +1,11 @@
 import config from './../config/config'
 import app from './express'
 import mongoose from 'mongoose'
+import fs from 'fs'
+import https from 'https'
+import http from 'http'
+
+
 
 // Connection URL
 mongoose.Promise = global.Promise
@@ -8,9 +13,32 @@ mongoose.connect(config.mongoUri, { useNewUrlParser: true, useCreateIndex: true,
 mongoose.connection.on('error', () => {
   throw new Error(`unable to connect to database: ${config.mongoUri}`)
 })
-app.listen(config.port, (err) => {
-  if (err) {
-    console.log(err)
-  }
-  console.info('Server started on port %s.', config.port)
-})
+// app.listen(config.port, (err) => {
+//   if (err) {
+//     console.log(err)
+//   }
+//   console.info('Server started on port %s.', config.port)
+// })
+
+const privateKey = fs.readFileSync('/etc/letsencrypt/live/www.covid.pucho.ai/privkey.pem', 'utf8');
+const certificate = fs.readFileSync('/etc/letsencrypt/live/www.covid.pucho.ai/cert.pem', 'utf8');
+const ca = fs.readFileSync('/etc/letsencrypt/live/www.covid.pucho.ai/fullchain.pem', 'utf8');
+
+const credentials = {
+	key: privateKey,
+	cert: certificate,
+	ca: ca
+};
+
+
+// Starting both http & https servers
+const httpServer = http.createServer(app);
+const httpsServer = https.createServer(credentials, app);
+
+httpServer.listen(80, () => {
+	console.log('HTTP Server running on port 80');
+});
+
+httpsServer.listen(443, () => {
+	console.log('HTTPS Server running on port 443');
+});
